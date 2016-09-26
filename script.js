7af5e594fb6369a4b117fa9dfdc73f412f9b25f3
briandesouza <brian.nunes.souza@gmail.com>
2016-09-26 07:40:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-$(document).ready(function() {
-    setInterval ('cursorAnimation()', 800);
+$(".timeline-item").hover(function () {
+    $(".timeline-item").removeClass("active");
+    $(this).toggleClass("active");
+    $(this).prev(".timeline-item").toggleClass("close");
+    $(this).next(".timeline-item").toggleClass("close");
 });
 
-function cursorAnimation() {
-    $('#cursor').animate({
-        opacity: 0
-    }, 'fast', 'swing').animate({
-        opacity: 1
-    }, 'fast', 'swing');
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
